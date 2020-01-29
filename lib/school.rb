d6e70c4148efbaa3b1ad4c9a3385a3595b55b084
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-29 13:07:51 -0700

    Initial commit

---
+++ lib/school.rb
@@ -0,0 +1,8 @@
+class School
+  attr_reader :start_time, :hours_in_school_day
+
+  def initialize(start_time_parameter, hours_in_school_day_parameter)
+    @start_time = start_time_parameter
+    @hours_in_school_day = hours_in_school_day_parameter
+  end
+end
