b00e964b0161150a71bae7aad101e5c531010adc
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-29 13:53:03 -0700

    Remove unnecessary end_time instance variable

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,11 @@
 class School
   attr_reader :start_time, :hours_in_school_day
-  attr_accessor :student_names, :end_time
+  attr_accessor :student_names
 
   def initialize(start_time_parameter, hours_in_school_day_parameter)
     @start_time = start_time_parameter
     @hours_in_school_day = hours_in_school_day_parameter
     @student_names = []
-    @end_time = ""
   end
 
   def add_student_name(student_parameter)
