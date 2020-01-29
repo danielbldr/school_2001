dd11afc469391beebb0485fbae9e8c5f0d30d1e8
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-29 13:11:37 -0700

    Add add_student_names method

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,15 @@
 class School
-  attr_reader :start_time, :hours_in_school_day, :student_names
+  attr_reader :start_time, :hours_in_school_day
+  attr_accessor :student_names
 
   def initialize(start_time_parameter, hours_in_school_day_parameter)
     @start_time = start_time_parameter
     @hours_in_school_day = hours_in_school_day_parameter
     @student_names = []
   end
+
+  def add_student_name(student_parameter)
+    @student_names << student_parameter
+  end
+
 end
